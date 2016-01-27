51a1890ad721b74ee0441e722407abe7d93ebf98
ewelshe <ewelsh@cca.edu>
2016-01-27 14:42:00 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,29 @@
+var  red=getColor();
+var blue=getColor();
+right();
+right();
+red=getColor();
+blue=getColor();
+if (getColor()=="blue"){
+  down();
+}else{
+  up();
+}
+right();
+right();
+getColor();
+if (getColor()=="blue"){
+  down();
+}else{
+  up();
+}
+right();
+right();
+if (getColor()=="blue"){
+  down();
+}else{
+  up();
+}
+right();
+right();
+right();
