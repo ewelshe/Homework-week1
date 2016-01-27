f09509585f83c956a608b570b89b3273082df271
ewelshe <ewelsh@cca.edu>
2016-01-27 14:51:22 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,21 @@
+var  red=getColor();
+var blue=getColor();
+right();
+if (getColor()=="red"){
+up();
+up();
+up();
+} else {
+  down();
+  down();
+  down();
+}
+
+if(getColor()=="red"){
+  left();
+  down();
+}else {
+  right();
+ down();
+ 
+}
