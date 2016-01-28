d41b8b47a183ad0a980afcbaf156d04896d4b26f
ewelshe <ewelsh@cca.edu>
2016-01-28 14:31:02 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,34 @@
+function downright(){
+  var count = 0;
+  while (count < 9) {
+    down();
+    count = count + 1;
+  }
+right();
+right();
+}  
+
+
+
+function upright(){
+  var count = 0;
+  while (count < 9) {
+    up();
+    count = count + 1;
+  }
+right();
+right();
+
+}
+downright();
+upright();
+downright();
+upright();
+ 
+var count = 0;
+  while (count < 8) {
+    down();
+    count = count + 1;
+  }
+right();
+right();
